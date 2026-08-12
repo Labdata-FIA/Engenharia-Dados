407871ea1ab8952c687692e91dcc9bab294e5ce5
nandorsilva <fernandos007@gmail.com>
2026-08-12 14:51:28 -0300

    demo

--- 17.Observabilidade/app-net/dev-kafka-demo/obj/Debug/net6.0/dev-kafka-demo.AssemblyInfo.cs
+++ 17.Observabilidade/app-net/dev-kafka-demo/obj/Debug/net6.0/dev-kafka-demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dev-kafka-demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+289609beb2ec5e71cb7b668cd2e7f5a67ee9c88e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+863500448008020c29d4b8ee20db99ed1dc7e7c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("dev-kafka-demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dev-kafka-demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
